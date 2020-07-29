c5962cc5a4a58e3acf2ba1ee8801b385b3686284
realralphg <mr.ralph01@gmail.com>
2020-07-29 11:45:03 +0100

    First Finalization of Product Page

--- src/components/Views/Products.vue
+++ src/components/Views/Products.vue
@@ -18,23 +18,8 @@
 
     <hr>
 
-    <h3>Basic CRUD Operations</h3>
-    <div class="products-test">
-      <div class="form-group">
-        <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
-      </div>
-
-      <div class="form-group">
-        <input type="text" placeholder="Product Price" v-model="product.price" class="form-control">
-      </div>  
-
-      <div class="form-group">
-        <button @click="savedata" class="btn btn-primary">Save Data</button>
-      </div>      
-    </div>
-
-
-    <h3>Product List</h3>  
+    <h3 class="d-inline-block">Product List</h3> 
+    <button @click="addNew" class="btn btn-primary float-right">Add Product</button> 
     <div class="table-responsive">
       <table class="table">
         <thead>
@@ -46,12 +31,12 @@
         </thead>
 
         <tbody>
-          <tr v-for="product in products" :key="product.index">
-            <td>{{product.data().name}}</td>
-            <td>  {{product.data().price}}</td>
-            <td> 
-              <button @click="editProduct(product)" class="btn btn-primary">Edit</button>
-              <button @click="deleteProduct(product.id)" class="btn btn-danger">Delete</button>              
+         <tr v-for="product in products" :key="product.index">
+            <td>{{product.name}}</td>
+            <td>{{product.price}}</td>
+            <td>
+              <button @click="editProduct(product)" type="button" class="btn btn-primary">Edit</button>
+              <button @click="deleteProduct(product)" type="button" class="btn btn-danger">Delete</button>
             </td>
           </tr>
         </tbody>
@@ -59,43 +44,85 @@
     </div>
 
     <!-- Edit Modal -->
-    <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="editLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="editLabel">Edit Product</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
+      <div class="modal fade" id="product" tabindex="-1" role="dialog" aria-labelledby="editLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="editLabel">Edit Product</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                  <!-- main product -->
+                  <div class="col-md-8">
+                    <div class="form-group">
+                      <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
+                    </div>
 
-          <div class="modal-body">
-            <div class="form-group">
-              <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
+                    <div class="form-group">
+                      <vue-editor v-model="product.description"></vue-editor>
+                    </div>
+                  </div>
+                  <!-- product sidebar -->
+                  <div class="col-md-4">
+                    <h4 class="display-6">Product Details</h4>
+                    <hr>
+
+                    <div class="form-group">
+                      <input type="text" placeholder="Product price" v-model="product.price" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                      <input type="text" @keyup.188="addTag" placeholder="Product tags" v-model="tag" class="form-control">
+                      
+                      <div  class="d-flex">
+                        <p v-for="tag in product.tags" :key="tag.index">
+                            <span class="p-1">{{tag}}</span>
+                        </p>
+                      </div>
+                    </div>
+
+                    <div class="form-group">
+                      <label for="product_image">Product Images</label>
+                      <input type="file" @change="uploadImage" class="form-control">
+                    </div>
+
+                    <div class="form-group d-flex">
+                      <div class="p-1" v-for="(image, index) in product.images" :key="image.index">
+                          <div class="img-wrapp">
+                              <img :src="image" alt="" width="80px">
+                             <span class="delete-img" @click="deleteImage(image, index)">x</span>
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>                
             </div>
 
-            <div class="form-group">
-              <input type="text" placeholder="Product Price" v-model="product.price" class="form-control">
-            </div> 
-          </div>
-
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button @click="updateProduct()" type="button" class="btn btn-primary">Save changes</button>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button @click="addProduct()" type="button" class="btn btn-primary" v-if="modal == 'new'">Save changes</button>
+              <button @click="updateProduct()" type="button" class="btn btn-primary" v-if="modal == 'edit'">Apply changes</button>
+            </div>
           </div>
         </div>
-      </div>
-    </div>    
+      </div>   
   </div>
 </template>
 
 <script>
 import {fb, db} from '@/firebase'
+import { VueEditor } from "vue2-editor"
 
 export default {
   name: "Products",
   props: {
     msg: String
+  },
+  components: {
+    VueEditor
   },
 
   data(){
@@ -103,85 +130,137 @@ export default {
       products: [],
       product: {
         name: null,
+        description: null,
         price: null,
+        tags: [],
+        images: [],
       },
-      activeItem: null
+      activeItem: null,
+      modal: null,
+      tag: null
+    }
+  },
+
+  // F I R E S T O R E 
+  firestore () {
+    return {
+      products: db.collection('products')
     }
   },
 
   methods: {
-    //  W A T C H E R 
-    watcher(){
-      db.collection('products').onSnapshot((querySnapshot) => {
-        this.products = []
-        querySnapshot.forEach((doc) => {
-          this.products.push(doc)        
-        })
-      })      
+    //  A D D N E W 
+    addNew(){
+      this.reset()
+      this.modal = 'new'
+      $('#product').modal('show')
     },
 
-    // R E A D D A T A
-    readData(){
-      db.collection('products').get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          this.products.push(doc)        
-        })
-      })      
+    // A D D  D A T A
+    addProduct(){
+      this.$firestore.products.add(this.product)
+      // N O T E ---> You can use SweetAlert *Toast* to display ADD success msg
+      // At Top right corner of screen instead of the code below for center msg
+      // Just Register SweetAlert Toast in Main.js - Dasall!      
+      Swal.fire(
+        'Added!',
+        'Added Successfully.',
+        'success'
+      )      
+      $('#product').modal('hide')
     },
 
-    // S A V E  D A T A
-    savedata(){
-      db.collection('products').add(this.product)
-      .then((docRef) => {
-        console.log('Document created with ID: ', docRef.id)
-        // this.reset()
+    // E D I T  D A T A
+    editProduct(product){
+      this.modal = 'edit'
+      this.product = product
+      $('#product').modal('show')
+    },
+
+    // U P D A T E  D A T A
+    updateProduct(){
+      this.$firestore.products.doc(this.product.id).update(this.product)
+      // T O A S T 
+      Toast.fire({
+        icon: 'success',
+        title: 'Updated successfully'
+      })     
+      $('#product').modal('hide')
+    },  
+
+    // D E L E T E  D A T A
+    deleteProduct(product){
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, Delete!'
+      }).then((result) => {
+        if (result.value) {
+          this.$firestore.products.doc(product.id).delete()           
+
+          Swal.fire(
+            'Deleted!',
+            'Deleted Successfully.',
+            'success'
+          )
+        }
       })
-      .catch((error) => {
-        console.error('Error adding document: ', error)
+    },
+
+    // T A G S
+    addTag(){
+      this.product.tags.push(this.tag)
+      this.tag = ''
+    },
+
+    uploadImage(e){
+      if(e.target.files[0]){
+        let file = e.target.files[0]
+        var storageRef = fb.storage().ref('products/'+ Math.random() + '_'  + file.name)
+        let uploadTask = storageRef.put(file)
+        uploadTask.on('state_changed', (snapshot) => {        
+        }, (error) => {
+          // Handle unsuccessful uploads 
+        }, () => {
+          // Handle successful uploads on complete
+          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+            //this.product.image = downloadURL
+            this.product.images.push(downloadURL)
+            console.log('DownloadURL', downloadURL)
+          })
+        })
+      }
+    },
+
+    deleteImage(img, index){
+      let image = fb.storage().refFromURL(img)
+      this.product.images.splice(index, 1)
+      image.delete().then(() => {
+        console.log('Image Deleted!')
+      }).catch((error) => {
+        console.log('Error Occured!')
       })
     },
 
     // R E S E T 
     reset(){
-      Object.assign(this.$data, this.$options.data.apply(this))
-    },
-
-    // E D I T  D A T A
-    editProduct(product){
-      $('#edit').modal('show')
-      this.product = product.data()
-      this.activeItem = product.id
-    },
-
-    // U P D A T E  D A T A
-    updateProduct(){
-      db.collection('products').doc(this.activeItem).update(this.product)
-      .then(() => {
-        console.log('Update Success')
-        this.watcher()
-        $('#edit').modal('hide')
-      }).catch((error) =>{
-        console.log('Update failed', error)
-      })
-    },    
-
-    // D E L E T E  D A T A
-    deleteProduct(id){
-      if(confirm('Really, Delete?')){
-        db.collection('products').doc(id).delete().then(() => {
-          console.log('Delete Successful')
-        }).catch((error) => {
-          console.log('Error deleting', error);
-        })
-      }else{
-
-      }
-    },
+      this.product = {
+        name: null,
+        description: null,
+        price: null,
+        tags: [],
+        images: [],
+      }   
+    } 
   },
 
   // C R E A T E D 
   created(){
-    this.readData()
+
   }
 }
 </script>
@@ -190,5 +269,19 @@ export default {
 <style scoped lang="scss">
 .btn{
   margin-right: 5px;
+}
+
+.img-wrapp{
+  position: relative;
+}
+
+.img-wrapp span.delete-img{
+  position: absolute;
+  top: -14px;
+  left: -2px;
+}
+
+.img-wrapp span.delete-img:hover{
+  cursor: pointer;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,35 @@ window.$ = window.jQuery = jQuery
 import 'popper.js'
 import './assets/app.scss'
 import {fb} from '@/firebase'
+import VueFirestore from 'vue-firestore'
+import Swal from 'sweetalert2'
+
+window.Swal = Swal
+
+
+Vue.use(VueFirestore, {
+    key: 'id',         // the name of the property. Default is '.key'.
+    enumerable: true  //  whether it is enumerable or not. Default is true.
+})
+Vue.use(VueFirestore)
+
+// T O A S T - SweetAlert
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  onOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
+window.Toast = Toast
+
+
+
 
 Vue.config.productionTip = false
 
